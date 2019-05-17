5e906d8866a071193761f4c494266b6bff36d03e
Neve0700 <nevzad.elezovic@gmail.com>
2019-05-17 04:54:30 +0200

    finnished project

--- thecoockiecorner/script/animationer.js
+++ thecoockiecorner/script/animationer.js
@@ -1,44 +1,42 @@
-
-
-$("#Hem").click(function(){
+$("#Hem").click(function() {
 
 
   $('html, body').animate({
-        scrollTop: $("#box1").offset().top-800
-    }, 2000);
+    scrollTop: $("#box1").offset().top - 800
+  }, 2000);
 
 
 
 });
 
-$("#omoss").click(function(){
+$("#omoss").click(function() {
 
 
   $('html, body').animate({
-        scrollTop: $("#box1").offset().top
-    }, 2000);
+    scrollTop: $("#box1").offset().top
+  }, 2000);
 
 
 
 });
 
-$("#recept").click(function(){
+$("#recept").click(function() {
 
 
   $('html, body').animate({
-        scrollTop: $("#box2").offset().top
-    }, 2000);
+    scrollTop: $("#box2").offset().top
+  }, 2000);
 
 
 
 });
 
-$("#alergi").click(function(){
+$("#alergi").click(function() {
 
 
   $('html, body').animate({
-        scrollTop: $("#box3").offset().top
-    }, 2000);
+    scrollTop: $("#box3").offset().top
+  }, 2000);
 
 
 
